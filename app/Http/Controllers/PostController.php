855059fab65f504553fe7224b600680afff245ac
huchani <daniel.huchani@uab.edu.bo>
2023-08-16 18:52:39 -0400

    subiendo proyecto de ralacion de tablas

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //
+
+
+    public function obtenercomentario(Request $request)
+    {
+        $comments = Post::find(1)->comments;
+  
+        dd($comments);
+    }
+
+    public function obtenerpost(Request $request)
+    {
+        $post = Comment::find(1)->post;
+  
+        dd($post);
+    }
+
+    public function agregarcomentario(Request $request)
+    {
+        $post = Post::find(1);
+   
+        $comment = new Comment;
+        $comment->comment = "Hi ItSolutionStuff.com";
+           
+        $post = $post->comments()->save($comment);
+    }
+
+    public function agregarnuevos(Request $request)
+    {
+        $post = Post::find(1);
+   
+        $comment1 = new Comment;
+        $comment1->comment = "Hi ItSolutionStuff.com Comment 1";
+           
+        $comment2 = new Comment;
+        $comment2->comment = "Hi ItSolutionStuff.com Comment 2";
+           
+        $post = $post->comments()->saveMany([$comment1, $comment2]);
+    }
+
+    public function asociar(Request $request)
+    {
+        $comment = Comment::find(1);
+        $post = Post::find(2);
+           
+        $comment->post()->associate($post)->save();
+    }
+}
